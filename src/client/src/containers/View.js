29b83f6d291fcbd423441a45a676920be9a959a7
503945930 <503945930@qq.com>
2016-08-29 23:11:42 +0800

    add chrome debug video

--- src/client/src/containers/View.js
+++ src/client/src/containers/View.js
@@ -22,32 +22,18 @@ class View extends Component{
           <div className=" col-md-offset-2 col-lg-offset-2 col-xs-12 col-sm-12 col-md-8 col-lg-8">
             <List>
               <ListItem
-                  primaryText="第一章 安装环境"
+                  primaryText="第一章 Chrome开发工具"
                   leftIcon={<ContentInbox />}
                   initiallyOpen={true}
                   primaryTogglesNestedList={true}
                   nestedItems={[
                     <ListItem
                       key={1}
-                      primaryText="1-1 下载文件，编辑器配置"
+                      primaryText="1-1 Chrome介绍"
 
                       containerElement={<Link  to={`/video`}></Link>}
                       leftIcon={<VideoCam />}
                       style={style.item}
-                    /> ,
-                    <ListItem
-                      key={2}
-                      primaryText="1-2 入门，编辑器配置"
-                      leftIcon={<VideoCam />}
-                      style={style.item}
-                      containerElement={<Link  to={`/video`}></Link>}
-                    />,
-                    <ListItem
-                      key={3}
-                      primaryText="1-3 下载文件，编辑器配置"
-                      leftIcon={<VideoCam />}
-                      style={style.item}
-                      containerElement={<Link  to={`/video`}></Link>}
                     />
                   ]}
                 />
